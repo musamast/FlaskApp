94a7bc900c4e5fe148b0b7d6832ac1186d307aa9
Muhammad Usama <musamast17@gmail.com>
2019-04-27 04:55:21 +0500

    2nd commit

--- app.py
+++ app.py
@@ -17,10 +17,10 @@ mysql = MySQL(app)
 app.config['UPLOAD_FOLDER'] = 'static/upload'
 
 # Config MySQL
-app.config['MYSQL_HOST'] = "localhost"
-app.config['MYSQL_USER'] = "root"
-app.config['MYSQL_PASSWORD'] = ""
-app.config['MYSQL_DB'] = "storedb"
+app.config['MYSQL_HOST'] = "us-cdbr-iron-east-02.cleardb.net"
+app.config['MYSQL_USER'] = "beeb27dc4ba1c5"
+app.config['MYSQL_PASSWORD'] = "d8a0ef01"
+app.config['MYSQL_DB'] = "heroku_725bdcfeff15a40"
 app.config['MYSQL_CURSORCLASS'] = "DictCursor"
 
 
