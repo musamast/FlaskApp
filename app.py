d4038f85feded9f11bc61e9930d29cc18f6b4ed1
Muhammad Usama <musamast17@gmail.com>
2019-04-27 05:49:12 +0500

    2nd committtt

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import os
+SECRET_KEY = os.urandom(32)
+app.config['SECRET_KEY'] = SECRET_KEY
 from flask import Flask, render_template, request, flash, redirect, url_for, session
 from flask_mysqldb import MySQL
 from passlib.hash import sha256_crypt
